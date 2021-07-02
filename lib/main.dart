9b5e385c49d7e38653c1de74ad82f325f897542d
afinajaharas <86813288+afinajaharas@users.noreply.github.com>
2021-07-01 19:27:06 -0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:uasmobile/landing.dart';
+import 'package:uasmobile/views/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'blog',
+      theme: ThemeData.light(),
+      home: Landing(),
+    );
+  }
+}
